039ee1e7be654885e066ec111a90081802ba47bd
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2025-03-19 15:24:05 -0600

    Created nice documentation for stuff

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
-
 @app.get("/api/")
-def read_root():
+def read_api():
     return {"Hello": "Daniel"}
 
 @app.get("/api/test/")
 def read_test():
     return {"We": "got this"}
+
+@app.get("/", response_class=FileResponse)
+def root():
+    return FileResponse("index.html")
